682e44e25f4ebd18558c210d2279c190ca5c2227
Alexander Hurtado Cardona <ahurtadoc@unal.edu.co>
2021-10-27 10:21:12 -0500

    Setup protractor configuration

---
+++ protractor/local.config.ts
@@ -0,0 +1,14 @@
+import { Config } from "protractor";
+
+const chromeConfig = {
+  browserName: 'chrome',
+  chromeOptions: { args: ["--headless", "--disable-gpu", "--window-size=800,600", "--remote-debugging-port=9222"] }
+}
+
+
+export const config: Config = {
+  capabilities: chromeConfig,
+  framework: 'jasmine',
+  specs: ['../test/google.spec.js'],
+  seleniumAddress: 'http://localhost:4444/wd/hub'
+};
